8ce6b0a53aeddb2e769264ee0e83343956e2e1dd
Serhii Izotov <sergiyizotov@gmail.com>
2023-01-23 18:44:38 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+const items = []
+
+galleryItems.forEach(element => {
+    const galleryItem = document.createElement('div')
+    galleryItem.className = 'gallery__item'
+    const galleryLink =document.createElement('a')
+    galleryLink.className = 'gallery__link'
+    galleryLink.href = element.original
+const galleryImage = document.createElement ('img')
+galleryImage.className ='gallery__image'
+galleryImage.src = element.preview
+galleryImage.setAttribute('data-source', element.original)
+galleryImage.alt = element.description
+
+galleryItem.append(galleryLink)
+galleryLink.append(galleryImage)
+items.push(galleryItem)
+})
+
+gallery.append(...items)
+
+
+
+
+
+
+
 console.log(galleryItems);
